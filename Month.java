3a6b7933068d0147d035fd075f30f22bbfe2acd8
ishfaatajunnesha29-cpu <ishfaatajunnesha29@gmail.com>
2025-10-07 19:27:04 +0530

    my first home work

---
+++ Month.java
@@ -0,0 +1,18 @@
+class Month
+{
+	public static void main(String[] args)
+	{
+		System.out.println("January");
+		System.out.println("February");
+		System.out.println("March");
+		System.out.println("April");
+		System.out.println("May");
+		System.out.println("June");
+		System.out.println("July");
+		System.out.println("August");
+		System.out.println("September");
+		System.out.println("October");
+		System.out.println("November");
+		System.out.println("December");
+	}
+}
