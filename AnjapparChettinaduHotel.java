ba6192dead0ab422f059023efb37c7fb7a8ed3bf
ishfaatajunnesha29-cpu <ishfaatajunnesha29@gmail.com>
2025-10-10 09:46:16 +0530

    09-10-23Third commit

---
+++ AnjapparChettinaduHotel.java
@@ -0,0 +1,26 @@
+class AnjapparChettinaduHotel
+{
+	public static void main(String[] args)
+	{
+		String foodName = "Mutton Biryani";
+		short foodAmount = 500;
+        String sideDish = "Mutton Chukka";
+		short dishAmount = 150;
+		String coolDrinks = "Apple Juice";
+		byte drinksAmount = 100;
+		String desert = "Butterscoth IceCream";
+		short desertAmount = 120;
+		short totalAmount = 870;
+		
+		System.out.println("Food Name   :"+foodName);
+        System.out.println("Amount      :"+foodAmount);
+		System.out.println("Side Dish   :"+sideDish);
+		System.out.println("Amount      :"+dishAmount);
+		System.out.println("Cool Drinks :"+coolDrinks);
+		System.out.println("Amount      :"+drinksAmount);
+		System.out.println("Desert      :"+desert);
+	    System.out.println("Amount      :"+desertAmount);
+	    System.out.println("Total Amount:"+totalAmount);
+	
+	}
+}
