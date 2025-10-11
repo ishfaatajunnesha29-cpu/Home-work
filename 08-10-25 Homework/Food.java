9e2ecbc743184c048da2683aef2ff901c9d0b48e
ishfaatajunnesha29-cpu <ishfaatajunnesha29@gmail.com>
2025-10-11 21:07:08 +0530

    Add files via upload

---
+++ 08-10-25 Homework/Food.java
@@ -0,0 +1,17 @@
+class Food
+{
+	public static void main(String[] args)
+	{
+		String foodName = "Paneer Butter Masala";
+		short calories = 400;
+		boolean isVegetarian = true;
+		float price = 250.50f;
+		
+		System.out.println(foodName);
+		System.out.println(calories);
+		System.out.println(isVegetarian);
+		System.out.println(price);
+		
+		
+	}
+}
