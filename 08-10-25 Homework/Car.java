96d00eb31d2ed5d66592ca2a4e8f5e2a858a93ce
ishfaatajunnesha29-cpu <ishfaatajunnesha29@gmail.com>
2025-10-11 21:06:23 +0530

    Add files via upload

---
+++ 08-10-25 Homework/Car.java
@@ -0,0 +1,21 @@
+class Car
+{
+	public static void main(String[] args)
+	{
+		String carName = "Tesla Model S";
+		short mileage = 396;
+		String fuelType = "Electric";
+		float price = 85000.00f;
+		String brand = "Tesla";
+
+        System.out.println(carName);		
+		System.out.println(brand);
+		System.out.println(fuelType);
+		System.out.println(mileage);
+		System.out.println(price);
+		
+		
+		
+	}
+	
+}
