c64e4c1cc46fceef4a203ea35f0a4bdd7a5ba95b
ishfaatajunnesha29-cpu <ishfaatajunnesha29@gmail.com>
2025-10-11 21:02:25 +0530

    Add files via upload

---
+++ 09-10-25 Homework/BioData.java
@@ -0,0 +1,26 @@
+class BioData
+{
+	public static void main(String[] args)
+	{
+		String name = "Tajunnesha";
+		char initial = 'B';
+		byte date = 29;
+		String month = "October";
+		short year = 2003;
+		byte age = 21;
+		String gender = "Female";
+		String area = "Madurai";
+		String fatherName = "Badhusha Mohideen";
+		char fatherInitial = 'G';
+		String motherName = "Fathima Zahara";
+		char motherInitial = 'B';
+		
+		System.out.println("Full Name    :"+name+" "+initial);
+		System.out.println("Date of Birth:"+date+" "+month+" "+year);
+		System.out.println("Age          :"+age);
+		System.out.println("Gender       :"+gender);
+		System.out.println("Father's Name:"+fatherName+" "+fatherInitial);
+	    System.out.println("Mother's Name:"+motherName+" "+motherInitial);
+    	System.out.println("Location     :"+area);
+	}
+}
