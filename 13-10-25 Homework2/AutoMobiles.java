aeb4400153e4eaffb4b76c613750c4feae3b47ec
ishfaatajunnesha29-cpu <ishfaatajunnesha29@gmail.com>
2025-10-14 20:46:05 +0530

    Add files via upload

---
+++ 13-10-25 Homework2/AutoMobiles.java
@@ -0,0 +1,13 @@
+class AutoMobiles
+{
+	public static void main(String[] args)
+	{
+		int amount = 750;
+		float percentage = 15;
+		
+		float commisionpercent = (percentage/100)*750;
+		
+		System.out.println("Rate of Auto Mobiles:"+" "+amount);
+		System.out.println("Commission Rate is:"+" "+commisionpercent);
+	}
+}
