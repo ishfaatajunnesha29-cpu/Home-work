56287df3b213d4cc421c3e7b0d6a18af29bd701a
ishfaatajunnesha29-cpu <ishfaatajunnesha29@gmail.com>
2025-10-22 12:42:09 +0530

    22-10-25

---
+++ 22-10-25 HomeWork/OddEven.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class OddEven
+{
+	public static void main(String[] args)
+	{
+		Scanner Sc=new Scanner(System.in);
+		System.out.println("Enter any Number");
+		int num=Sc.nextInt();
+		
+		if(num%2==1)
+		{
+			System.out.println("The given Number is Odd");
+			
+		}
+		else if(num%2==0)
+		{
+			System.out.println("The given Number is Even");
+		}
+		else
+		{
+			System.out.println("You are Entered a wrong Number");
+		}
+	}
+}
