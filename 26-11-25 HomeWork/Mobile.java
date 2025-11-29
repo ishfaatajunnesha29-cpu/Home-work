20c7a80360ad16c13e7a100e8fe157c623a18cb7
ishfaatajunnesha29-cpu <ishfaatajunnesha29@gmail.com>
2025-11-29 18:16:42 +0530

    Add files via upload

---
+++ 26-11-25 HomeWork/Mobile.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SmartDevices;
+
+/**
+ *
+ * @author ISHFAA TAJ
+ */
+public class Mobile extends Device
+{
+    @Override
+    void powerOn()
+    {
+        System.out.println("Mobile is now On");
+    }
+}
