410f973d82da10bfe0522b8a31dbd3854a3f1261
ishfaatajunnesha29-cpu <ishfaatajunnesha29@gmail.com>
2026-03-20 05:21:38 +0530

    Add files via upload

---
+++ JS_homeWork/Hw20-03-26.js
@@ -0,0 +1,11 @@
+const name = "Tajunnesha"        
+let age = 22           
+let student = true               
+const favoriteColor = "Purple"   
+
+
+console.log("--- Details ---")
+console.log('Name:           '+name)
+console.log('Age:            '+age)
+console.log('Student Status: '+student)
+console.log('Favorite Color: '+favoriteColor)
